d31d08f40d50d04b1c3ac38184a6d0baa451278b
Kaya <kayalion@kayalion.net>
2015-08-19 16:34:42 +0200

    added jsnoapi response

--- src/ride/web/rest/controller/UploadController.php
+++ src/ride/web/rest/controller/UploadController.php
@@ -2,6 +2,9 @@
 
 namespace ride\web\rest\controller;
 
+use ride\library\http\jsonapi\JsonApi;
+use ride\library\http\Header;
+use ride\library\http\Response;
 use ride\library\system\exception\FileSystemException;
 
 use ride\service\UploadService;
@@ -19,28 +22,30 @@ class UploadController extends AbstractController {
      * @return null
      */
     public function uploadAction(UploadService $uploadService) {
+        $api = new JsonApi();
+        $document = $api->createDocument();
+
         $file = $this->request->getBodyParameter('file');
         if (!is_array($file)) {
-            $this->response->setBadRequest();
-            $this->setJsonView(array('error' => 'no file uploaded'));
+            $document->addError($api->createError(Response::STATUS_CODE_BAD_REQUEST, 'file.upload.none', 'No file uploaded'));
+        } else {
+            try {
+                $uploadedFile = $uploadService->uploadFile($file);
 
-            return;
+                $resource = $api->createResource('uploads', $uploadedFile->getName());
+                $resource->setAttribute('name', $uploadedFile->getName());
+                $resource->setAttribute('mime', $file['type']);
+                $resource->setAttribute('size', $file['size']);
+
+                $document->setResourceData('uploads', $resource);
+            } catch (FileSystemException $exception) {
+                $document->addError($api->createError(Response::STATUS_CODE_BAD_REQUEST, 'file.upload.error', 'Error occured while processing the file upload', $exception->getMessage()));
+            }
         }
 
-        try {
-            $uploadedFile = $uploadService->uploadFile($file);
-        } catch (FileSystemException $exception) {
-            $this->response->setBadRequest();
-            $this->setJsonView(array('error' => $exception->getMessage()));
-
-            return;
-        }
-
-        $this->setJsonView(array(
-            'file' => $uploadedFile->getName(),
-            'type' => $file['type'],
-            'size' => $file['size'],
-        ));
+        $this->setJsonView($document);
+        $this->response->setStatusCode($document->getStatusCode());
+        $this->response->setHeader(Header::HEADER_CONTENT_TYPE, JsonApi::CONTENT_TYPE);
     }
 
 }
